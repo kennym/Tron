b214502817ec28b984c969f5ee951902b349abf7
Elvis Ratzlaff <eratzlaff@adelantos.com.py>
2013-10-20 11:34:31 -0300

    Inicio para registro

--- public_html/js/controller/registro.js
+++ public_html/js/controller/registro.js
@@ -1,5 +1,12 @@
 'use strict';
 function RegistroCtrl($scope, $rootScope, $http, $location) {
+    $scope.cliente = {};
+
+    $scope.registrar = function() {
+        $http.post('/api/cliente', $scope.cliente, {}).success(function(susess) {
+            alert('Registro');
+        });
+    };
+
 
 }
-
